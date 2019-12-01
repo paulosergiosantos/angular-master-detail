efa140046204a111316d956ef2d92a92bc5fe7bf
Paulo Sergio <pauloserigo.natercia@gmail.com>
2019-12-01 14:45:42 -0300

    Category model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,13 +1,14 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Category } from './pages/categories/shared/category.model';
 
 export class InMemoryDatabase implements InMemoryDbService {
     createDb(reqInfo?: import("angular-in-memory-web-api").RequestInfo): {} | import("rxjs").Observable<{}> | Promise<{}> {
-       const categories = [
-        { id: 1, name: 'Lazer', description: 'Cinema, parque, praia, etc'},
-        { id: 2, name: 'Supermercado', description: 'Alimentação, Higiene, etc'},
-        { id: 3, name: 'Saúde', description: 'Plano Saúde, Dentista, Medicamentos, etc'}
-       ];
+        const categories: Category[] = [
+            { id: 1, name: 'Lazer', description: 'Cinema, parque, praia, etc' },
+            { id: 2, name: 'Supermercado', description: 'Alimentação, Higiene, etc' },
+            { id: 3, name: 'Saúde', description: 'Plano Saúde, Dentista, Medicamentos, etc' }
+        ];
 
-       return { categories };
+        return { categories };
     }
 }
